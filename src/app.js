e5f571abafedf4d22b2c923dcbc48cbe26d09371
Vickygonrod <159072629+Vickygonrod@users.noreply.github.com>
2024-03-01 20:01:23 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,54 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
 
-window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["A shark", "A thief", "My dog", "A cat", "A comedian", "A Karen"];
+  let what = [
+    " bit my ",
+    " ate my ",
+    " threw my ",
+    " hit my ",
+    " stole my ",
+    " crashed my ",
+    " broke my "
+  ];
+  let which = [
+    "car ",
+    "leg ",
+    "homework ",
+    "laptop ",
+    "spirit ",
+    "will to live "
+  ];
+  let when = [
+    "last night.",
+    "yesterday.",
+    "last week.",
+    "5 minutes ago.",
+    "last Friday.",
+    "just now."
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whichIndex = Math.floor(Math.random() * which.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    "" +
+    what[whatIndex] +
+    "" +
+    which[whichIndex] +
+    "" +
+    when[whenIndex]
+  );
 };
